8cb31d98a6c78db8216daee42184ed9060ccc906
bruce.woods <bruce.woods@burlingtonstores.com>
2018-11-11 11:31:55 -0500

    My changes up to 09-NOV 18:00 EST

--- src/main/java/tudelft/chocolate/ChocolateBags.java
+++ src/main/java/tudelft/chocolate/ChocolateBags.java
@@ -1,5 +1,12 @@
 package tudelft.chocolate;
 
+/*
+  Package can store specified mass built from discrete smaller components.
+  Components of package can be units of 1 or 5.
+  Use multiples of 5 first, followed by multiples of 1 to achieve specified mass.
+  Return a sentinal value of -1 if specified mass cannot delivered with on-hand components specified.
+ */
+
 public class ChocolateBags {
 
     public int calculate(int small, int big, int total) {
